47a53f5d37f8769ef0d1355812d57028192959c9
wangkodok <wangkodok@gmail.com>
2024-03-19 19:06:54 +0900

    리덕스 툴킷 state 연결

--- src/reduxToolkitStore/index.js
+++ src/reduxToolkitStore/index.js
@@ -1,5 +1,4 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
@@ -22,6 +21,8 @@ const counterSlice = createSlice({
   },
 });
 
-const store = createStore(counterSlice.reducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
