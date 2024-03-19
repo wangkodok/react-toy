dd9b112f13adf2013e694ccd4cb157bf0f89b6e5
wangkodok <wangkodok@gmail.com>
2024-03-19 18:57:26 +0900

    리덕스 툴킷 설치하고 createSlice 코드

---
+++ src/reduxToolkitStore/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter = state.counter + 1;
+    },
+    decrement(state) {
+      state.counter = state.counter - 1;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const store = createStore(counterSlice.reducer);
+
+export default store;
