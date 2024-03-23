7d237c666dca19e65db96cd47d2893ef36aa66cc
wangkodok <wangkodok@gmail.com>
2024-03-23 22:16:22 +0900

    다른 방법으로 페이지 라우팅

--- src/RouterPage.jsx
+++ src/RouterPage.jsx
@@ -1,17 +1,31 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/products",
-    element: <ProductsPage />,
-  },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/products",
+//     element: <ProductsPage />,
+//   },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 export default function RouterPage() {
   return <RouterProvider router={router} />;
