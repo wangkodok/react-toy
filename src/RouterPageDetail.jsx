61c6362ea2dba261f3c60fbe055170bd57b94aea
wangkodok <wangkodok@gmail.com>
2024-03-25 18:43:54 +0900

    동적 라우트에 링크 추가

--- src/RouterPageDetail.jsx
+++ src/RouterPageDetail.jsx
@@ -1,4 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+  },
+];
 
 export default function RouterPageDetail() {
   const params = useParams();
@@ -10,15 +25,13 @@ export default function RouterPageDetail() {
       <h1># 상세 페이지</h1>
       <p>{params.productId}</p>
       <ul>
-        <li>
-          <a href="">Detail 1</a>
-        </li>
-        <li>
-          <a href="">Detail 2</a>
-        </li>
-        <li>
-          <a href="">Detail 3</a>
-        </li>
+        {PRODUCTS.map((prod) => {
+          return (
+            <li key={prod.id}>
+              <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
