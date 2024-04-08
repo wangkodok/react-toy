2c00c98e633496149f14c96678a6174aa7d9a194
wangkodok <wangkodok@gmail.com>
2024-04-08 17:40:40 +0900

    코드를 정리하기 위한 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,75 @@
 import MainPage from "./MainPage/MainRouter";
-import Button from "./Button";
-import Props from "./Props";
-import PropsChildren from "./PropsChildren";
-import AccordionMenu from "./AccordionMenu";
-import Conditional from "./Conditional";
-import Map from "./Map";
-import ComponentPropsChildren from "./ComponentPropsChildren";
-import ComponentTag from "./ComponentTag";
-import StateParent from "./StateParent";
-import StateParentProps from "./StateParentProps";
-import StyledComponents from "./StyledComponents";
-import TailwindCSS from "./TailwindCSS";
-import UseRef from "./UseRef";
-import Project from "./Project";
-import UserInput from "./UserInput";
-import StateList from "./StateList";
-import ContextAPI from "./ContextAPI";
-import UseReducer from "./UseReducer";
-import UseReducerModal from "./UseReducerModal";
-import UseEffectMount from "./UseEffectMount";
-import UseEffectDependencyArray from "./UseEffectDependencyArray";
-import UseCallbackSetTimeout from "./UseCallbackSetTimeout";
-import UseCallbackParentComponent from "./UseCallbackParentComponent";
-import UseCallbackParentBox from "./UseCallbackChildBox/UseCallbackParentBox";
-import SetTimeOut from "./SetTimeOut";
-import SetInterval from "./SetInterval";
+// import Button from "./Button";
+// import Props from "./Props";
+// import PropsChildren from "./PropsChildren";
+// import AccordionMenu from "./AccordionMenu";
+// import Conditional from "./Conditional";
+// import Map from "./Map";
+// import ComponentPropsChildren from "./ComponentPropsChildren";
+// import ComponentTag from "./ComponentTag";
+// import StateParent from "./StateParent";
+// import StateParentProps from "./StateParentProps";
+// import StyledComponents from "./StyledComponents";
+// import TailwindCSS from "./TailwindCSS";
+// import UseRef from "./UseRef";
+// import Project from "./Project";
+// import UserInput from "./UserInput";
+// import StateList from "./StateList";
+// import ContextAPI from "./ContextAPI";
+// import UseReducer from "./UseReducer";
+// import UseReducerModal from "./UseReducerModal";
+// import UseEffectMount from "./UseEffectMount";
+// import UseEffectDependencyArray from "./UseEffectDependencyArray";
+// import UseCallbackSetTimeout from "./UseCallbackSetTimeout";
+// import UseCallbackParentComponent from "./UseCallbackParentComponent";
+// import UseCallbackParentBox from "./UseCallbackChildBox/UseCallbackParentBox";
+// import SetTimeOut from "./SetTimeOut";
+// import SetInterval from "./SetInterval";
 // import Redux from "./Redux";
 // import ReduxToolkit from "./ReduxToolkit";
 // import ReduxToolkitLoginLogout from "./ReduxToolkitLoginLogout";
-import RouterPage from "./RouterPage";
-import InputHandle from "./InputHandling/InputHandle";
-import InputHandleList from "./InputHandling/InputHandleList";
-import ReduxStudy from "./reduxStoreStudy/ReduxStudy";
+// import RouterPage from "./RouterPage";
+// import InputHandle from "./InputHandling/InputHandle";
+// import InputHandleList from "./InputHandling/InputHandleList";
+// import ReduxStudy from "./reduxStoreStudy/ReduxStudy";
 
 export default function App() {
   return (
     <div>
       <MainPage />
-      <Button />
-      <Props />
-      <PropsChildren />
-      <AccordionMenu />
-      <Conditional />
-      <Map />
-      <ComponentPropsChildren />
-      <ComponentTag />
-      <StateParent />
-      <StateParentProps />
-      <StyledComponents />
-      <TailwindCSS />
-      <UseRef />
-      <Project />
-      <UserInput />
-      <StateList />
-      <ContextAPI />
-      <UseReducer />
-      <UseReducerModal />
-      <UseEffectMount />
-      <UseEffectDependencyArray />
-      <UseCallbackSetTimeout />
-      <UseCallbackParentComponent />
-      <UseCallbackParentBox />
-      <SetTimeOut />
-      <SetInterval />
+      {/* <Button /> */}
+      {/* <Props /> */}
+      {/* <PropsChildren /> */}
+      {/* <AccordionMenu /> */}
+      {/* <Conditional /> */}
+      {/* <Map /> */}
+      {/* <ComponentPropsChildren /> */}
+      {/* <ComponentTag />/ */}
+      {/* <StateParent /> */}
+      {/* <StateParentProps /> */}
+      {/* <StyledComponents /> */}
+      {/* <TailwindCSS /> */}
+      {/* <UseRef /> */}
+      {/* <Project /> */}
+      {/* <UserInput /> */}
+      {/* <StateList /> */}
+      {/* <ContextAPI /> */}
+      {/* <UseReducer /> */}
+      {/* <UseReducerModal /> */}
+      {/* <UseEffectMount /> */}
+      {/* <UseEffectDependencyArray /> */}
+      {/* <UseCallbackSetTimeout /> */}
+      {/* <UseCallbackParentComponent /> */}
+      {/* <UseCallbackParentBox /> */}
+      {/* <SetTimeOut /> */}
+      {/* <SetInterval /> */}
       {/* <Redux /> */}
       {/* <ReduxToolkit /> */}
       {/* <ReduxToolkitLoginLogout /> */}
-      <RouterPage />
-      <InputHandle />
-      <InputHandleList />
-      <ReduxStudy />
+      {/* <RouterPage /> */}
+      {/* <InputHandle /> */}
+      {/* <InputHandleList /> */}
+      {/* <ReduxStudy /> */}
     </div>
   );
 }
