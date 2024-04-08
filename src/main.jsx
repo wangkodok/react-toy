7964240accd34818e15b2d19bafe87d0f08d1f50
wangkodok <wangkodok@gmail.com>
2024-04-08 17:15:23 +0900

    CSS 초기화

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Reset } from "styled-reset";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 // import store from "./reduxStore/index.js";
@@ -9,6 +10,7 @@ import store from "./reduxStoreStudy/index.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
+      <Reset />
       <App />
     </React.StrictMode>
   </Provider>
